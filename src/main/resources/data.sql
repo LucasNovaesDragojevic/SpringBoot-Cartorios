0d7efef6760511cd4a62eb2fbea2f8bcf53bf038
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-08-22 21:30:53 -0300

    Created model and database config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into cartorio (name) values ('cartório regional 022');
+insert into cartorio (name) values ('cartório regional 058');
+insert into cartorio (name) values ('cartório regional 074');
